8a34433e6c58ca71df3fdee93eb65cddea0d187b
Joseph Sedivy <sedivyj0322@my.uwstout.edu>
2020-11-16 22:14:52 -0600

    Added Dao Functions for New Tables

--- app/src/main/java/com/example/skilltracker/database/SkillsRepository.kt
+++ app/src/main/java/com/example/skilltracker/database/SkillsRepository.kt
@@ -3,8 +3,9 @@ package com.example.skilltracker.database
 import android.app.Application
 import androidx.lifecycle.LiveData
 import com.example.skilltracker.database.dao.SkillDao
-import com.example.skilltracker.database.entity.SkillSet
+import com.example.skilltracker.database.entity.*
 
+// TODO: Determine what should be cached later
 /**
  * uses Coroutines seen by the "suspend" keyword
  *
@@ -21,13 +22,48 @@ class SkillsRepository(app: Application){
         allSkillSets = skillsDao.getAll()
     }
 
+    /* QUERIES */
     fun getSkillSet(): LiveData<List<SkillSet>> {
         return allSkillSets
     }
 
-    suspend fun insertSkillSet(SkillSet: SkillSet){
-        skillsDao.insert(SkillSet)
+    fun getSkills(): LiveData<List<Skill>> {
+        return skillsDao.getAllSkills()
     }
 
+    fun getTasks(): LiveData<List<Task>> {
+        return skillsDao.getAllTasks()
+    }
+
+    fun getSkillSetWithSkills(): LiveData<List<SkillSetWithSkills>> {
+        return skillsDao.getAllSkillSetWithSkills()
+    }
+
+    fun getSkillWithTasks(): LiveData<List<SkillWithTasks>> {
+        return skillsDao.getAllSkillWithTasks()
+    }
+
+    /* INSERTS */
+    suspend fun insertSkillSet(skillSet: SkillSet){
+        skillsDao.insert(skillSet)
+    }
+
+    suspend fun insertSkill(skill: Skill){
+        skillsDao.insert(skill)
+    }
+
+    suspend fun insertTask(task: Task){
+        skillsDao.insert(task)
+    }
+
+    suspend fun insertSkillSetWithSkills(skillSetWithSkills: SkillSetWithSkills){
+        skillsDao.insert(skillSetWithSkills)
+    }
+
+    suspend fun insertSkillsWithTasks(skillWithTasks: SkillWithTasks){
+        skillsDao.insert(skillWithTasks)
+    }
+
+    /* DELETES */
     suspend fun nukeTable() = skillsDao.nukeTable()
 }
--- app/src/main/java/com/example/skilltracker/database/dao/SkillDao.kt
+++ app/src/main/java/com/example/skilltracker/database/dao/SkillDao.kt
@@ -2,26 +2,108 @@ package com.example.skilltracker.database.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.*
-import com.example.skilltracker.database.entity.SkillSet
+import com.example.skilltracker.database.entity.*
 
-
+// TODO: Add updates and deletes as needed
 /**
  * adding suspend to functions that need to run asynchronously
  */
 @Dao
 interface SkillDao {
+
+    /* QUERIES */
+    /**
+     * Return all SkillSet data from database
+     * @return LiveData with list of SkillSets
+     */
     @Query("SELECT * FROM SkillSet")
     fun getAll(): LiveData<List<SkillSet>>
 
+    /**
+     * Deletes all data in SkillSet table
+     */
     @Query("DELETE FROM SkillSet")
     suspend fun nukeTable()
 
+    /**
+     * Return all Skills data from database
+     * @return LiveData with list of Skills
+     */
+    @Query("SELECT * FROM Skill")
+    fun getAllSkills(): LiveData<List<Skill>>
+
+    /**
+     * Return all Task data from database
+     * @return LiveData with list of Tasks
+     */
+    @Query("SELECT * FROM Task")
+    fun getAllTasks(): LiveData<List<Task>>
+
+    /**
+     * Return Join data between Skillset and Skill tables from database
+     * @return LiveData with list of SkillSet with related Skill
+     */
+    @Query("SELECT * FROM SkillSet")
+    @Transaction
+    fun getAllSkillSetWithSkills(): LiveData<List<SkillSetWithSkills>>
+
+    /**
+     * Return Join data between Skill and Task tables from database
+     * @return LiveData with list of Skill with related Tasks
+     */
+    @Query("SELECT * FROM Skill")
+    @Transaction
+    fun getAllSkillWithTasks(): LiveData<List<SkillWithTasks>>
+
+    /* INSERTS */
+    /**
+     * Insert SkillSet data into database
+     * @param skillSet SkillSets to be added to SkillSet table
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(vararg SkillSets: SkillSet)
+    suspend fun insert(vararg skillSet: SkillSet)
 
+    /**
+     * Insert Skill data into database
+     * @param skill Skills to be added to Skill table
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(vararg skill: Skill)
+
+    /**
+     * Insert Task data into database
+     * @param task Task to be added to Task table
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(vararg task: Task)
+
+    /**
+     * Insert join data between Skillset and Skill into database
+     * @param skillSetWithSkills join data be added to SkillSetSkillCrossRef table
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(vararg skillSetWithSkills: SkillSetWithSkills)
+
+    /**
+     * Insert SkillSet data into database
+     * @param skillWithTasks join data to be added to SkillTaskCrossRef table
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(vararg skillWithTasks: SkillWithTasks)
+
+    /* DELETES */
+    /**
+     * Delete SkillSet data from database
+     * @param skillSet SkillSets to be removed from SkillSet table
+     */
     @Delete
-    suspend fun delete(SkillSet: SkillSet)
+    suspend fun delete(skillSet: SkillSet)
 
+    /* UPDATES */
+    /**
+     * Update SkillSet data in database
+     * @param skillSets SkillSet rows to be updated
+     */
     @Update
-    suspend fun update(vararg SkillSets: SkillSet)
+    suspend fun update(vararg skillSets: SkillSet)
 }
