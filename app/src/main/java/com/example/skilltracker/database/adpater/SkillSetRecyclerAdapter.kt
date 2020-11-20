befd3dd3fa35e98279d2b16daa83ae316e16c362
Joseph Sedivy <sedivyj0322@my.uwstout.edu>
2020-11-19 23:17:39 -0600

    Renaming and Formatting Changed list of SkillSets from "orders" to "skillSets"

--- app/src/main/java/com/example/skilltracker/database/adpater/SkillSetRecyclerAdapter.kt
+++ app/src/main/java/com/example/skilltracker/database/adpater/SkillSetRecyclerAdapter.kt
@@ -21,10 +21,10 @@ import com.example.skilltracker.database.entity.SkillSet
  * fill in those views within the layout.
  *
  * @param context a private context
- * @param orders a list of previous orders
+ * @param skillSets a list of previous SkillSets
  * @property layoutInflater for inflating the recycler view
  */
-class SkillSetRecyclerAdapter(private val context: Context, private var orders: List<SkillSet>) :
+class SkillSetRecyclerAdapter(private val context: Context, private var skillSets: List<SkillSet>) :
     PagingDataAdapter<SkillSet, SkillSetRecyclerAdapter.ViewHolder>(SkillSetDiffCallBack()) {
 
     private val layoutInflater = LayoutInflater.from(context)
@@ -61,7 +61,7 @@ class SkillSetRecyclerAdapter(private val context: Context, private var orders: 
      * @param position the position of the previously ordered SkillSet in the previous orders list
      */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val skillSet = orders[position]
+        val skillSet = skillSets[position]
         holder.SkillSetName?.text = skillSet.name
         holder.skillsetDescription?.text = skillSet.description
         holder.dateCreated?.text = skillSet.dateCreated.toLocalDate().toString()
@@ -88,20 +88,20 @@ class SkillSetRecyclerAdapter(private val context: Context, private var orders: 
      *
      * @return the size of the previous orders list
      */
-    override fun getItemCount() = orders.size
+    override fun getItemCount() = skillSets.size
 
 }
 
-/**
- *  Boiler plate code for the Paging Data Adaptor to work, which is not really used yet but
- *  is for better practices when adding data to the database I think.
- */
-private class SkillSetDiffCallBack : DiffUtil.ItemCallback<SkillSet>() {
-    override fun areItemsTheSame(oldItem: SkillSet, newItem: SkillSet): Boolean {
-        return oldItem.skillSetId == newItem.skillSetId
-    }
+    /**
+     *  Boiler plate code for the Paging Data Adaptor to work, which is not really used yet but
+     *  is for better practices when adding data to the database I think.
+     */
+    private class SkillSetDiffCallBack : DiffUtil.ItemCallback<SkillSet>() {
+        override fun areItemsTheSame(oldItem: SkillSet, newItem: SkillSet): Boolean {
+            return oldItem.skillSetId == newItem.skillSetId
+        }
 
-    override fun areContentsTheSame(oldItem: SkillSet, newItem: SkillSet): Boolean {
-        return oldItem == newItem
-    }
+        override fun areContentsTheSame(oldItem: SkillSet, newItem: SkillSet): Boolean {
+            return oldItem == newItem
+        }
 }
