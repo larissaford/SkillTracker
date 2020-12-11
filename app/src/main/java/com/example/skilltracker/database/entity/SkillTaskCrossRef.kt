7d118d6505b3fc1597b79525207167eeac5b6f30
Joseph Sedivy <sedivyj0322@my.uwstout.edu>
2020-12-10 19:55:57 -0600

    Finishing Comments for Entities in Database

--- app/src/main/java/com/example/skilltracker/database/entity/SkillTaskCrossRef.kt
+++ app/src/main/java/com/example/skilltracker/database/entity/SkillTaskCrossRef.kt
@@ -4,6 +4,8 @@ import androidx.room.Entity
 
 /**
  * Data class that sets up relationship between Skill and Task tables
+ * @property skillId id of Skill for relationship
+ * @property taskId id of Task for relationship
  */
 @Entity(primaryKeys = ["skillId", "taskId"])
 data class SkillTaskCrossRef(
